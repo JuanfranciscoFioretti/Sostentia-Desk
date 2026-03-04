e39a4baab76b649ae7b4ca2e895abf80e2a27b58
Fioretti Juan Francisco <juanfrafio125@gmail.com>
2026-03-05 00:49:00 +0100

    Fix: Return locale from getRequestConfig

--- i18n/request.ts
+++ i18n/request.ts
@@ -1,5 +1,5 @@
 import { getRequestConfig } from 'next-intl/server';
 
-export default getRequestConfig(async () => {
-  return {};
+export default getRequestConfig(async ({ locale }) => {
+  return { locale };
 });
